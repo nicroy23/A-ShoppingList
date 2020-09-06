fe7bd75cd8edb2f87c2e9b17b98b5de034fee744
nic_roy23 <nic5.roy23@gmail.com>
2020-09-06 18:01:59 -0400

    Updated documentation. Finished for now.

--- src/app/add-item/add-item.component.ts
+++ src/app/add-item/add-item.component.ts
@@ -15,12 +15,22 @@ export class AddItemComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  /**
+   * Function to add the list item to the list. Gets called on button/enterKey click
+   * 
+   * @param name - The name/content of the list item to add
+   */
   addItemToList(name: string): void {
     if (name !== '') {
       this.itemsService.items.push({ id: this.randomId(), name: name, checked: false });
     }
   }
 
+  /**
+   * Function that generates a random id of length 6 for the item. Not too complicated because the id is internat with a specific list
+   * 
+   * @return The random id generated
+   */
   randomId(): string {
     var result = '';
     var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
--- src/app/list-item/list-item.component.ts
+++ src/app/list-item/list-item.component.ts
@@ -9,8 +9,14 @@ import { ItemsService } from '../items.service';
 })
 export class ListItemComponent implements OnInit {
 
+  /**
+   * Gets the list item from the parent component (list in this case)
+   */
   @Input() item: {id: string, name: string, checked: boolean};
 
+  /**
+   * Outputs the event message from the toggleDone function. Needed to emit messages to other components that will receive the event
+   */
   @Output() messageEvent = new EventEmitter<number>();
 
   constructor(private itemsService: ItemsService) { }
@@ -19,6 +25,9 @@ export class ListItemComponent implements OnInit {
     
   }
 
+  /**
+   * Function to announce that the 'check' button has been pressed to the other components that need to know
+   */
   toggleDone(): void {
     this.itemsService.updateItemCheck(this.item.id, !this.item.checked);
     this.messageEvent.emit(this.itemsService.getProgress());
